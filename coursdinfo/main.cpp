6f0efee82fbde385792ccf1a81c3a38a22e79057
Yvon Perez <yvon.perez@etu.minesparis.psl.eu>
2024-11-05 16:23:22 +0100

    Initial Commit

---
+++ coursdinfo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  coursdinfo
+//
+//  Created by Yvon Perez on 05/11/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
