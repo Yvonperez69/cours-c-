052fc0901378090e7c24275a350b2e034eae50d0
Yvon Perez <yvon.perez@etu.minesparis.psl.eu>
2024-11-07 11:00:59 +0100

    Ajout de l'algo de tri

--- coursdinfo/main.cpp
+++ coursdinfo/main.cpp
@@ -1,14 +1,26 @@
-//
-//  main.cpp
-//  coursdinfo
-//
-//  Created by Yvon Perez on 05/11/2024.
-//
-
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "my sort algorithm" << std::endl;
+    int tab[] = {16,5,8,34,89};
+
+    for (int i=0; i<4; i=i+1) {
+        int i_min = i;
+        for (int j = i+1; j<5; j= j+1) {
+            if (tab[i_min] > tab[j]) {
+                i_min = j;
+            }
+        }
+        if (i_min != i){
+            int temp =tab[i];
+            tab[i] = tab[i_min];
+            tab[i_min] = temp;
+            }
+        }
+    
+    
+    for (int k = 0; k < 5; k=k+1) {
+        std::cout << tab[k] << " ";
+    }
     return 0;
 }
