d0f84b7d95bb18929938ec59007ccf122ef3e8ea
Yvon Perez <yvon.perez@etu.minesparis.psl.eu>
2024-11-12 15:24:09 +0100

    modification de la stack

--- coursdinfo/stackint.cpp
+++ coursdinfo/stackint.cpp
@@ -4,28 +4,31 @@ struct IntStack10{
     
 private :
     
-    int stack [10];
-    int i_sommet; // Indice du sommet de la pile que j'initialise à -1
+    int * stack;
+    int i_sommet;
     
 public :
     
-    bool empty() {
-        return i_sommet == -1;
-    }
-    bool full() {
-        return i_sommet == 9;
-    }
-    
-    void init() {
+    void init(int taille) {
+        stack = new int[taille];
         i_sommet = -1;
     }
     
-    void push(int i) {
+    bool empty() const {
+        return i_sommet == -1;
+    }
+    bool full() const {
+        return i_sommet == 9;
+    }
+    
+
+    
+    void push(int val) {
         if (full()) {
             std::cout << "Erreur : La pile est pleine." << std::endl;
         } else {
-            stack[++i_sommet] = i;
-            std::cout << i << " a été ajouté à la pile." << std::endl;
+            stack[++i_sommet] = val;
+            std::cout << val <<" a été ajouté à la pile." << std::endl;
         }
     }
     int pop() {
@@ -37,33 +40,34 @@ public :
         }
     }
     
-    void print() {
+   void print() const {
         if (empty()) {
             std::cout << "La pile est vide." << std::endl;
         } else {
-            std::cout << "Éléments dans la pile : ";
+            std::cout << "Éléments dans la pile : " << std::endl;
             for (int k = 0; k <= i_sommet; k++) {
                 std::cout << stack[k] << " ";
             }
-            std::cout << std::endl;
         }
+       std::cout << " " << std::endl;
     }
 };
 
 
 int main() {
     IntStack10 stack;
-    stack.init();
+    stack.init(10);
 
     stack.push(1);
     stack.push(2);
     stack.push(3);
 
     stack.print();
-
+    
     std::cout << "Élément retiré : " << stack.pop() << std::endl;
-
+    
     stack.print();
 
+    
     return 0;
 }
